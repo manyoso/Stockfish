5009085a6ca5d6b640dda192fbfed088625cc78f
Adam Treat <adam.treat@qt.io>
2021-01-29 09:47:34 -0500

    Only reduce on main thread.

--- src/search.cpp
+++ src/search.cpp
@@ -964,7 +964,8 @@ namespace {
     // Step 10. If the position is not in TT, decrease depth by 2
     if (   PvNode
         && depth >= 6
-        && !ttMove)
+        && !ttMove
+        && thisThread == Threads.main())
         depth -= 2;
 
 moves_loop: // When in check, search starts from here
